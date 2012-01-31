4c51af104ae1c397447f31b1206a505adc7e4025
nicl <nl@sftreal.co.za>
2012-01-31 10:36:20 +0200

    My HelloNetduino project

---
+++ src/HelloNetduino/HelloNetduino/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Netduino.Lib.Common;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace HelloNetduino
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            OnBoardLED onboardLED = new OnBoardLED();
+            var led = new LED(Pins.GPIO_PIN_D7);
+
+            while (true)
+            {
+                SOSBlink(onboardLED);
+
+                led.LedState = LedState.On;
+                Thread.Sleep(1000);
+
+                led.LedState = LedState.Off;
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void SOSBlink(LED led)
+        {
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+
+            led.LedState = LedState.On;
+            Thread.Sleep(250);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(250);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(250);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+            led.LedState = LedState.On;
+            Thread.Sleep(450);
+            led.LedState = LedState.Off;
+            Thread.Sleep(250);
+        }
+    }
+}
