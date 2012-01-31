f66c27fa9cd06403ab1691d8606734b3223e0a96
nicl <nl@sftreal.co.za>
2012-01-31 10:47:43 +0200

    Knock Sensor

---
+++ src/Neduino.KnockSensor/Neduino.KnockSensor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Netduino.Lib.Common;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Neduino.KnockSensor
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+
+            var knockSensor = new PiezoKnockSensor(Pins.GPIO_PIN_A0);
+
+            while (true)
+            {
+                var soundLevel = knockSensor.Read();
+                Debug.Print(soundLevel.ToString());
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
