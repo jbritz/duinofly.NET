000ae6ef1629a4a5c913bebc01bfb7d9a6e52221
nicl <nl@sftreal.co.za>
2012-01-31 10:48:43 +0200

    SD Card I/O

---
+++ src/Netduino.SDCardIO/Netduino.SDCardIO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO;
+
+namespace Netduino.SDCardIO
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DirectoryInfo rootDirectory = new DirectoryInfo(@"\SD\");
+            RecurseFolders(rootDirectory);
+        }
+
+        private static void RecurseFolders(DirectoryInfo directory)
+        {
+            if (directory.Exists)
+            {
+                Debug.Print(directory.FullName);
+
+                //foreach (FileInfo file in directory.GetFiles())
+                //{
+                //    Debug.Print(file.FullName);
+                //   // File.Delete(file.FullName);
+                //}
+
+
+                //foreach (DirectoryInfo subDirectory in directory.GetDirectories())
+                //{
+                //   RecurseFolders(subDirectory);
+                //}
+            }
+        }
+    }
+}
