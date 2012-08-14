ce71031b26b6b0e4ff14ac8bdf35c4fc7a020e5f
nicl <nl@sftreal.co.za>
2012-08-14 16:03:53 +0200

    6  DOF progress

---
+++ src/Netduino.Lib.SixDOF/Netduino.App.SixDOF/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Netduino.Lib.SixDOF.Domain;
+
+namespace Netduino.App.SixDOF
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            ADXL345 adxl = new ADXL345();
+            adxl.Init(ADXL345.ADXL345_ADDR_ALT_LOW);
+
+            while (true)
+            {
+                Thread.Sleep(150);
+
+                var results = new int[3];
+                adxl.ReadAccel(out results);
+
+                Debug.Print(results[0].ToString());
+            }
+        }
+
+    }
+}
